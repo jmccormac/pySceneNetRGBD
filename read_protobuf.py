a32585bb6b5b6ccb65a7740e23b509c50bdfc8eb
John McCormac <brendan.mccormac13@imperial.ac.uk>
2016-12-15 19:22:50 +0000

    Add comments

--- read_protobuf.py
+++ read_protobuf.py
@@ -1,10 +1,31 @@
 import scenenet_pb2 as sn
-from nltk.corpus import wordnet as wn
+import os
+
+# TODO Update the data_root_path to you data directory
+data_root_path = '/path/to/your/data/val'
+protobuf_path = 'data/scenenet_rgbd_val.pb'
+
+# These functions produce a file path (on Linux systems) to the image given
+# a view and render path from a trajectory.  As long the data_root_path to the
+# root of the dataset is given.  I.e. to either val or train
+def photo_path_from_view(render_path,view):
+    photo_path = os.path.join(render_path,'photo')
+    image_path = os.path.join(photo_path,'{0}.jpg'.format(view.frame_num))
+    return os.path.join(data_root_path,image_path)
+
+def instance_path_from_view(render_path,view):
+    photo_path = os.path.join(render_path,'instance')
+    image_path = os.path.join(photo_path,'{0}.png'.format(view.frame_num))
+    return os.path.join(data_root_path,image_path)
+
+def depth_path_from_view(render_path,view):
+    photo_path = os.path.join(render_path,'instance')
+    image_path = os.path.join(photo_path,'{0}.png'.format(view.frame_num))
+    return os.path.join(data_root_path,image_path)
 
 trajectories = sn.Trajectories()
-file_path = 'data/scenenet_rgbd_val.pb'
 try:
-    with open(file_path,'rb') as f:
+    with open(protobuf_path,'rb') as f:
         trajectories.ParseFromString(f.read())
 except IOError:
     print('Scenenet protobuf data not found at location:{0}'.format(file_path))
@@ -20,6 +41,15 @@ for traj in trajectories.trajectories:
     print('='*20)
     print('')
     print('Number of instances: {0}'.format(len(traj.instances)))
+    '''
+    The instances attribute of trajectories contains all of the information
+    about the different instances.  The instance.instance_id attribute provides
+    correspondences with the rendered instance.png files.  I.e. for a given
+    trajectory, if a pixel is of value 1, the information about that instance,
+    such as its type, semantic class, and wordnet id, is stored here.
+    For more information about the exact information available refer to the
+    scenenet.proto file.
+    '''
     for instance in traj.instances:
         instance_type = sn.Instance.InstanceType.Name(instance.instance_type)
         print('='*20)
@@ -36,6 +66,14 @@ for traj in trajectories.trajectories:
         print('-'*20)
         print('')
     print('Render path:{0}'.format(traj.render_path))
+    '''
+    The views attribute of trajectories contains all of the information
+    about the rendered frames of a scene.  This includes camera poses,
+    frame numbers and timestamps.
+    '''
     for view in traj.views:
+        print(photo_path_from_view(traj.render_path,view))
+        print(depth_path_from_view(traj.render_path,view))
+        print(instance_path_from_view(traj.render_path,view))
         print(view)
     break
