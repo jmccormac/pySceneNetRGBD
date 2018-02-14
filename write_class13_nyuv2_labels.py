cadbf3d1ecce10d17102ecc275ac53a4a078742f
ankur handa <ankurhanda@ankurs-MacBook-Pro.local>
2018-02-14 13:12:34 +0000

    adding code to convert to 13 class nyuv2
    Signed-off-by: ankur handa <ankurhanda@ankurs-MacBook-Pro.local>

--- write_class13_nyuv2_labels.py
+++ write_class13_nyuv2_labels.py
@@ -1,9 +1,10 @@
-from PIL import Image
-import numpy as np
-import os
-import pathlib
-import random
 import scenenet_pb2 as sn
+import os
+import numpy as np
+from PIL import Image
+
+data_root_path = 'data/val'
+protobuf_path = 'data/scenenet_rgbd_val.pb'
 
 NYU_13_CLASSES = [(0,'Unknown'),
                   (1,'Bed'),
@@ -76,15 +77,28 @@ NYU_WNID_TO_CLASS = {
     '02992529':7, '03222722':12, '04373704':4, '02851099':13, '04061681':10, '04529681':7,
 }
 
-data_root_path = 'data/val'
-protobuf_path = 'data/scenenet_rgbd_val.pb'
+# These functions produce a file path (on Linux systems) to the image given
+# a view and render path from a trajectory.  As long the data_root_path to the
+# root of the dataset is given.  I.e. to either val or train
+def photo_path_from_view(render_path,view):
+    photo_path = os.path.join(render_path,'photo')
+    image_path = os.path.join(photo_path,'{0}.jpg'.format(view.frame_num))
+    return os.path.join(data_root_path,image_path)
 
 def instance_path_from_view(render_path,view):
     photo_path = os.path.join(render_path,'instance')
     image_path = os.path.join(photo_path,'{0}.png'.format(view.frame_num))
     return os.path.join(data_root_path,image_path)
 
-def save_class_from_instance(instance_path,class_path, class_NYUv2_colourcode_path, mapping):
+def depth_path_from_view(render_path,view):
+    photo_path = os.path.join(render_path,'depth')
+    image_path = os.path.join(photo_path,'{0}.png'.format(view.frame_num))
+    return os.path.join(data_root_path,image_path)
+
+def save_class_from_instance(instance_path,
+                             class_path,
+                             class_NYUv2_colourcode_path,
+                             mapping):
     instance_img = np.asarray(Image.open(instance_path))
     class_img = np.zeros(instance_img.shape)
     h,w  = instance_img.shape
@@ -109,6 +123,7 @@ def save_class_from_instance(instance_path,class_path, class_NYUv2_colourcode_pa
     class_img.save(class_path)
     class_img_rgb.save(class_NYUv2_colourcode_path)
 
+
 if __name__ == '__main__':
     trajectories = sn.Trajectories()
     try:
@@ -118,21 +133,66 @@ if __name__ == '__main__':
         print('Scenenet protobuf data not found at location:{0}'.format(data_root_path))
         print('Please ensure you have copied the pb file to the data directory')
 
-    traj = random.choice(trajectories.trajectories)
-    instance_class_map = {}
-    for instance in traj.instances:
+    print('Number of trajectories:{0}'.format(len(trajectories.trajectories)))
+    for traj in trajectories.trajectories:
 
-        instance_type = sn.Instance.InstanceType.Name(instance.instance_type)
+        instance_class_map = {}
+        for instance in traj.instances:
+            instance_type = sn.Instance.InstanceType.Name(instance.instance_type)
+            if instance.instance_type != sn.Instance.BACKGROUND:
+                instance_class_map[instance.instance_id] = NYU_WNID_TO_CLASS[instance.semantic_wordnet_id]
 
-        if instance.instance_type != sn.Instance.BACKGROUND:
-            instance_class_map[instance.instance_id] = NYU_WNID_TO_CLASS[instance.semantic_wordnet_id]
+        # layout_type = sn.SceneLayout.LayoutType.Name(traj.layout.layout_type)
+        # layout_path = traj.layout.model
+        # print('='*20)
+        # print('Render path:{0}'.format(traj.render_path))
+        # print('Layout type:{0} path:{1}'.format(layout_type,layout_path))
+        # print('='*20)
+        # print('')
+        # print('Number of instances: {0}'.format(len(traj.instances)))
+        '''
+        The instances attribute of trajectories contains all of the information
+        about the different instances.  The instance.instance_id attribute provides
+        correspondences with the rendered instance.png files.  I.e. for a given
+        trajectory, if a pixel is of value 1, the information about that instance,
+        such as its type, semantic class, and wordnet id, is stored here.
+        For more information about the exact information available refer to the
+        scenenet.proto file.
+        '''
+        # for instance in traj.instances:
+        #     instance_type = sn.Instance.InstanceType.Name(instance.instance_type)
+        #     print('='*20)
+        #     print('Instance id:{0}'.format(instance.instance_id))
+        #     print('Instance type:{0}'.format(instance_type))
+        #     if instance.instance_type != sn.Instance.BACKGROUND:
+        #         print('Wordnet id:{0}'.format(instance.semantic_wordnet_id))
+        #         print('Plain english name:{0}'.format(instance.semantic_english))
+        #     if instance.instance_type == sn.Instance.LIGHT_OBJECT:
+        #         light_type = sn.LightInfo.LightType.Name(instance.light_info.light_type)
+        #         print('Light type:{0}'.format(light_type))
+        #     if instance.instance_type == sn.Instance.RANDOM_OBJECT:
+        #         print('Object info:{0}'.format(instance.object_info))
+        #     print('-'*20)
+        #     print('')
+        # print('Render path:{0}'.format(traj.render_path))
+        '''
+        The views attribute of trajectories contains all of the information
+        about the rendered frames of a scene.  This includes camera poses,
+        frame numbers and timestamps.
+        '''
+        for view in traj.views:
+            print(photo_path_from_view(traj.render_path,view))
 
-    for view in traj.views:
+            instance_path = instance_path_from_view(traj.render_path,view)
+            class_path = instance_path.replace('instance','class13')
+            class_NYUv2_colourcode_path = instance_path.replace('instance', 'class13colour')
 
-        instance_path = instance_path_from_view(traj.render_path,view)
-        print('Converting instance image:{0} to class image'.format(instance_path))
+            save_class_from_instance(instance_path_from_view(traj.render_path,view),
+                                     class_path,
+                                     class_NYUv2_colourcode_path,
+                                     instance_class_map)
 
-        save_class_from_instance(instance_path,'semantic_class.png','NYUv2.png',instance_class_map)
-        print('Breaking early and writing class to semantic_class.png')
-
+            # print(depth_path_from_view(traj.render_path,view))
+            # print(instance_path_from_view(traj.render_path,view))
+            # print(view)
         break
